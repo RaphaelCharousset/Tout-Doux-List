79d8706d22d884f181903446f5522eb511748e79
Raphael Charousset <raphael.charousset@gmail.com>
2021-08-28 15:17:05 +0200

    sort by undone and done tasks

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -3,14 +3,21 @@ import Task from './Task/Task'
 
 import './Tasks.scss'
 
-const Tasks = ({ darkMode, tasks }) => (
+const Tasks = ({ darkMode, tasks }) => {
+  const undoneTasks = tasks.filter(task => !task.done)
+  const doneTasks = tasks.filter(task => task.done)
+
+  return (
   <main className={ darkMode ? "main main--dark" : "main main--light" } >
     <div className="main__container">
       {
-        tasks.map( task => <Task key={task.id} {...task} darkMode={darkMode} />)
+        undoneTasks.map( task => <Task key={task.id} {...task} darkMode={darkMode} />)
+      }
+      {
+        doneTasks.map( task => <Task key={task.id} {...task} darkMode={darkMode} />)
       }
     </div>
   </main>
-)
+)}
 
 export default Tasks
